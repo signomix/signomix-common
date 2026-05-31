3ba6f6c909cf8b2de448f4dedc40c183cace493f
gskorupa <g.skorupa@gmail.com>
2026-05-31 12:26:31 +0200

    Add restoreDb and refactor SchedulerDao

--- src/main/java/com/signomix/common/tsdb/SchedulerDao.java
+++ src/main/java/com/signomix/common/tsdb/SchedulerDao.java
@@ -1,15 +1,12 @@
 package com.signomix.common.tsdb;
 
+import com.signomix.common.db.IotDatabaseException;
+import com.signomix.common.db.SchedulerDaoIface;
+import io.agroal.api.AgroalDataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-
 import org.jboss.logging.Logger;
-
-import com.signomix.common.db.IotDatabaseException;
-import com.signomix.common.db.SchedulerDaoIface;
-
-import io.agroal.api.AgroalDataSource;
 
 public class SchedulerDao implements SchedulerDaoIface {
 
@@ -22,57 +19,101 @@ public class SchedulerDao implements SchedulerDaoIface {
         this.dataSource = ds;
     }
 
-        @Override
+    @Override
     public void backupDb() throws IotDatabaseException {
-        String query = "COPY task_definition to '/var/lib/postgresql/data/export/task_definition.csv' DELIMITER ';' CSV HEADER;"
-                + "COPY task_parameter to '/var/lib/postgresql/data/export/task_parameter.csv' DELIMITER ';' CSV HEADER;";
-        try (Connection conn = dataSource.getConnection();
-                PreparedStatement pstmt = conn.prepareStatement(query);) {
+        String query =
+            "COPY task_definition to '/var/lib/postgresql/data/export/task_definition.csv' DELIMITER ';' CSV HEADER;" +
+            "COPY task_parameter to '/var/lib/postgresql/data/export/task_parameter.csv' DELIMITER ';' CSV HEADER;";
+        try (
+            Connection conn = dataSource.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(query);
+        ) {
             pstmt.execute();
         } catch (SQLException e) {
             logger.error("Error during backup", e);
-            throw new IotDatabaseException(IotDatabaseException.SQL_EXCEPTION, e.getMessage(), e);
+            throw new IotDatabaseException(
+                IotDatabaseException.SQL_EXCEPTION,
+                e.getMessage(),
+                e
+            );
         } catch (Exception e) {
             logger.error("Error during backup", e);
-            throw new IotDatabaseException(IotDatabaseException.UNKNOWN, e.getMessage());
+            throw new IotDatabaseException(
+                IotDatabaseException.UNKNOWN,
+                e.getMessage()
+            );
         }
+    }
 
+    public void restoreDb() throws IotDatabaseException {
+        String query =
+            "COPY task_definition FROM '/var/lib/postgresql/data/import/task_definition.csv' DELIMITER ';' CSV HEADER;" +
+            "COPY task_parameter FROM '/var/lib/postgresql/data/import/task_parameter.csv' DELIMITER ';' CSV HEADER;";
+        try (
+            Connection conn = dataSource.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(query);
+        ) {
+            pstmt.execute();
+        } catch (SQLException e) {
+            logger.error("Error during restore", e);
+            throw new IotDatabaseException(
+                IotDatabaseException.SQL_EXCEPTION,
+                e.getMessage(),
+                e
+            );
+        } catch (Exception e) {
+            logger.error("Error during restore", e);
+            throw new IotDatabaseException(
+                IotDatabaseException.UNKNOWN,
+                e.getMessage()
+            );
+        }
     }
 
     @Override
     public void createStructure() throws IotDatabaseException {
-                // Table task_definition holds TaskDefinition objects
-        String query = "CREATE TABLE IF NOT EXISTS task_definition ("
-                + "id BIGSERIAL PRIMARY KEY,"
-                + "type INT NOT NULL,"
-                + "userid VARCHAR,"
-                + "enabled BOOLEAN NOT NULL,"
-                + "nl_schedule_definition VARCHAR(1024),"
-                + "schedule_definition VARCHAR(255),"
-                + "description VARCHAR(255),"
-                + "organization INT"
-                + ");";
+        // Table task_definition holds TaskDefinition objects
+        String query =
+            "CREATE TABLE IF NOT EXISTS task_definition (" +
+            "id BIGSERIAL PRIMARY KEY," +
+            "type INT NOT NULL," +
+            "userid VARCHAR," +
+            "enabled BOOLEAN NOT NULL," +
+            "nl_schedule_definition VARCHAR(1024)," +
+            "schedule_definition VARCHAR(255)," +
+            "description VARCHAR(255)," +
+            "organization INT" +
+            ");";
 
-        try (Connection connection = dataSource.getConnection();
-                var statement = connection.createStatement()) {
+        try (
+            Connection connection = dataSource.getConnection();
+            var statement = connection.createStatement()
+        ) {
             statement.execute(query);
         } catch (Exception e) {
-            throw new RuntimeException("Error creating table task_definition", e);
+            throw new RuntimeException(
+                "Error creating table task_definition",
+                e
+            );
         }
 
         // Table task_parameter holds the parameters for the tasks
-        String query2 = "CREATE TABLE IF NOT EXISTS task_parameter ("
-                + "task_id BIGINT NOT NULL,"
-                + "name VARCHAR(32) NOT NULL,"
-                + "value VARCHAR(1024) NOT NULL"
-                + ");";
-        try (Connection connection = dataSource.getConnection();
-                var statement = connection.createStatement()) {
+        String query2 =
+            "CREATE TABLE IF NOT EXISTS task_parameter (" +
+            "task_id BIGINT NOT NULL," +
+            "name VARCHAR(32) NOT NULL," +
+            "value VARCHAR(1024) NOT NULL" +
+            ");";
+        try (
+            Connection connection = dataSource.getConnection();
+            var statement = connection.createStatement()
+        ) {
             statement.execute(query2);
         } catch (Exception e) {
-            throw new RuntimeException("Error creating table task_parameter", e);
+            throw new RuntimeException(
+                "Error creating table task_parameter",
+                e
+            );
         }
-
     }
-
 }
